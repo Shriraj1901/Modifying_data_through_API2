574100a4a1918a2800b1acff62559d79a4591903
shreeraj <shreerajjadhav19@gmail.com>
2025-02-06 14:26:47 +0530

    code upadeted

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+const PORT = process.env.PORT || 3010;
 
-app.use(express.static('static'));
+
+
+app.use(bodyParser.json());
+
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  res.send('Welcome to the Restaurant Menu API');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
